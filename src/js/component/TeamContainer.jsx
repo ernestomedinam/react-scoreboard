937d10c8c540157dd38aebc89c23640e51aa09d6
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2024-02-07 21:37:03 -0400

    :books: Adds team name feature using refs

--- src/js/component/TeamContainer.jsx
+++ src/js/component/TeamContainer.jsx
@@ -1,15 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 
 export const TeamContainer = (props) => {
     // {mode: "home" | "away"}
     const [score, setScore] = useState(0);
+    const [teamName, setTeamName] = useState();
+    const nameInput = useRef();
     return (
         <div className="d-flex flex-column py-3 px-0 mx-3">
-            <input 
-                className="form-control"
-                type="text" 
-                placeholder={"name for " + props.mode + " team"} />
+            {teamName ? (
+                <h2 className="text-light display-4">{teamName}</h2>
+            ) : (
+                <React.Fragment>
+                    <input 
+                        ref={nameInput}
+                        className="form-control"
+                        type="text" 
+                        placeholder={"name for " + props.mode + " team"} />
+                    <button
+                        onClick={(event) => {
+                            // get a hold of the input
+                            console.log(">>> 💚 this is the value of the input:", nameInput.current.value);
+                            // setTeamName(input.value);
+                            setTeamName(nameInput.current.value);
+                            nameInput.current.value = "";
+                        }} 
+                        className="btn btn-warning my-2">
+                        add team
+                    </button>
+                </React.Fragment>
+            )}
             <p className="score-big text-white text-center">
                 {score}
             </p>
